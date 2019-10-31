d1de9a14304ed02bf7323d7dd97d435226f818d6
Ciaran Fontein <ciaranfontein@gmail.com>
2019-10-31 15:45:37 -0700

    🐬🎸:Fixture data initalizes with 8 users, 7 of them named after aerosmith members

---
+++ imports/api/orders.js
@@ -0,0 +1,21 @@
+import { Mongo } from "meteor/mongo";
+
+Meteor.methods({
+  //Use this syntax to add order when we have a select recipient button
+  //Meteor.call("orders.addOrder", 1, 2, 30);
+  "orders.addOrder"(ownerId, recipientId, maxDeliveryDays) {
+    if (!this.ownerId) {
+      throw new Meteor.Error(
+        "orders.addOrder.not-authorized",
+        "User needs to be logged in to add Orders"
+      );
+    }
+    Orders.insert({
+      owner: ownerId,
+      recipient: recipientId,
+      status: "In Transit",
+      maxDeliveryDays: maxDeliveryDays
+    });
+  }
+});
+export const Orders = new Mongo.Collection("orders");
