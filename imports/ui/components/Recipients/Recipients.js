7a7ef1ffffabc82fb9db1cea523e1ea7beb87517
Ciaran Fontein <ciaranfontein@gmail.com>
2019-10-31 12:13:32 -0700

    📦: Folder structure and files created for the Recipients page

---
+++ imports/ui/components/Recipients/Recipients.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+
+const Recipients = () => {
+  return <div>Recipients</div>;
+};
+
+export default withStyles(styles)(Recipients);
