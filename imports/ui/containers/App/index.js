5937ba8c10ed2f070b12bc392674863d50ee153b
Ciaran Fontein <ciaranfontein@gmail.com>
2019-11-04 14:56:34 -0800

    🤡: Recipients are listed in the recipient list

--- imports/ui/containers/App/index.js
+++ imports/ui/containers/App/index.js
@@ -19,10 +19,6 @@ class _App extends Component {
   render() {
     return (
       <Router>
-<<<<<<< HEAD
-=======
-        <Recipient />
->>>>>>> 84138577a932a7b55228feb9a1f712b559e0e021
         <Routes />
       </Router>
     );
