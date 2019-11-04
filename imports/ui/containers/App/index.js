61b3556b03539ce63295ab016886bee2651f9ce3
Alex Faissal <alefaissal@gmail.com>
2019-11-04 14:22:05 -0800

    Fix index App file

--- imports/ui/containers/App/index.js
+++ imports/ui/containers/App/index.js
@@ -6,48 +6,29 @@ import AccountLogin from "../../pages/AccountLogin";
 // import Container from "@material-ui/core/Container";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "../../routes";
-<<<<<<< HEAD
-import OthersOrdersListItem from "../../components/OthersOrdersListItem";
-=======
 import Recipient from "../../pages/Recipients";
 import MenuBar from "../../components/MenuBar";
->>>>>>> 3e73ce27e93dc81533ecf2b2d7dde542f2315746
 
 class _App extends Component {
-  constructor() {
-    super();
+   constructor() {
+      super();
 
-    this.state = {};
-  }
+      this.state = {};
+   }
 
-<<<<<<< HEAD
    render() {
       return (
-         <OthersOrdersListItem />
-         // {<Router>
-         //    <Routes />
-         // </Router>}
+         <Router>
+            <Routes />
+         </Router>
       );
    }
-=======
-  render() {
-    return (
-      <Router>
-<<<<<<< HEAD
-=======
-        <Recipient />
->>>>>>> 84138577a932a7b55228feb9a1f712b559e0e021
-        <Routes />
-      </Router>
-    );
-  }
->>>>>>> 3e73ce27e93dc81533ecf2b2d7dde542f2315746
 }
 
 export default App = withTracker(() => {
-  return {
-    // todos: ToDos.find({}).fetch(),
-    currentUser: Meteor.user(),
-    currentUserId: Meteor.userId()
-  };
+   return {
+      // todos: ToDos.find({}).fetch(),
+      currentUser: Meteor.user(),
+      currentUserId: Meteor.userId()
+   };
 })(_App);
