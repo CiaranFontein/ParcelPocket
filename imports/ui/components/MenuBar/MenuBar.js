3057e0cd56777a81586cf2a7ee4ab6bbced59608
Ciaran Fontein <ciaranfontein@gmail.com>
2019-11-04 10:04:07 -0800

    🦎: Settings.json is added to the .gitignore

--- imports/ui/components/MenuBar/MenuBar.js
+++ imports/ui/components/MenuBar/MenuBar.js
@@ -1,5 +1,55 @@
 import React from "react";
 
-const Header = () => <div>MenuBar</div>;
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.flexToolbar}>
+        <Link to="./items">
+          <IconButton
+            className={classes.iconButton}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <img src={logo} alt="logo" width="40px" />
+          </IconButton>
+        </Link>
+        <div className={classes.rightSideIcons}>
+          <Link to="./share">
+            <Button className={classes.iconButton}>
+              <AddCircle />
+              Share Something
+            </Button>
+          </Link>
+          <IconButton
+            onClick={handleClick}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={classes.iconButton}
+          >
+            <MoreVert />
+          </IconButton>
+          <Menu
+            id="long-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            {options.map((option, index) => (
+              <Link key={index} to={option.path}>
+                <MenuItem to={option.path} onClick={handleClose}>
+                  <ListItemIcon>{option.icon}</ListItemIcon>
+                  <Typography noWrap>{option.name}</Typography>
+                </MenuItem>
+              </Link>
+            ))}
+          </Menu>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
--- imports/ui/components/MenuBar/styles.js
+++ imports/ui/components/MenuBar/styles.js
@@ -1,3 +1,8 @@
-const styles = theme => ({});
+const styles = theme => ({
+  flexToolBar: {
+    display: "flex",
+    justifyContent: "space-between"
+  }
+});
 
 export default styles;
