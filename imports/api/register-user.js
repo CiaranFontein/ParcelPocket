4beb35af596dbc4ec9ed08b9ed0f6324f0885d49
Ciaran Fontein <ciaranfontein@gmail.com>
2019-11-06 11:16:08 -0800

    🎺: register-user now takes location coordinates and placeId

--- imports/api/register-user.js
+++ imports/api/register-user.js
@@ -1,27 +1,29 @@
 import { Accounts } from "meteor/accounts-base";
 
 Meteor.methods({
-   "users.addUser"(user) {
-      console.log(user);
-      if (
-         Meteor.users
-            .find({ emails: { $elemMatch: { address: user.email } } })
-            .count()
-      ) {
-         throw new Meteor.Error(
-            "users.addUser.not-authorized",
-            "This email is already in the register, please try to login"
-         );
+  "users.addUser"(user) {
+    console.log(user);
+    if (
+      Meteor.users
+        .find({ emails: { $elemMatch: { address: user.email } } })
+        .count()
+    ) {
+      throw new Meteor.Error(
+        "users.addUser.not-authorized",
+        "This email is already in the register, please try to login"
+      );
+    }
+    Accounts.createUser({
+      email: user.email,
+      password: user.password,
+      profile: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        address: user.address,
+        reciever: user.receiver[0],
+        location: user.location,
+        placeId: user.placeId
       }
-      Accounts.createUser({
-         email: user.email,
-         password: user.password,
-         profile: {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            address: user.address,
-            reciever: user.receiver[0]
-         }
-      });
-   }
+    });
+  }
 });
