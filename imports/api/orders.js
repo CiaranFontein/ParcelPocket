9beeb9e771b2ee4ceb430a972b324d53713b2a8c
Alex Faissal <alefaissal@gmail.com>
2019-11-06 11:58:11 -0800

    Add action for buttons in parcel page

--- imports/api/orders.js
+++ imports/api/orders.js
@@ -1,4 +1,5 @@
 import { Mongo } from "meteor/mongo";
+import { Meteor } from "meteor/meteor";
 export const Orders = new Mongo.Collection("orders");
 
 if (Meteor.isServer) {
@@ -18,22 +19,16 @@ Meteor.methods({
       });
    },
    "orders.changeStatusToDelivered"(order) {
-      if (order.owner !== this.userId) {
-         throw new Meteor.Error(
-            "todos.toggleComplete.not-authorized",
-            "You are not allowed to change status."
-         );
+      if (order.recipient !== Meteor.userId()) {
+         throw new Meteor.Error("You are not allowed to change status.");
       }
       Orders.update(order._id, {
          $set: { status: "Delivered" }
       });
    },
    "orders.changeStatusToCompleted"(order) {
-      if (order.owner !== this.userId) {
-         throw new Meteor.Error(
-            "todos.toggleComplete.not-authorized",
-            "You are not allowed to change status."
-         );
+      if (order.recipient !== Meteor.userId()) {
+         throw new Meteor.Error("You are not allowed to change status.");
       }
       Orders.update(order._id, {
          $set: { status: "Completed" }
