5b254df14ad29d600df6816e77b0dd64c8e004bc
Ciaran Fontein <ciaranfontein@gmail.com>
2019-11-07 13:30:08 -0800

    👨‍👦‍: Merge is being resolved

--- imports/ui/pages/RegisterPage/styles.js
+++ imports/ui/pages/RegisterPage/styles.js
@@ -1,15 +1,6 @@
 import { height } from "@material-ui/system";
 
-<<<<<<< HEAD
 const styles = theme => ({
-  page: {
-    backgroundImage:
-      "url(" +
-      "https://images.unsplash.com/photo-1512418490979-92798cec1380?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" +
-      ")",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-  },
   registerContainer: {
     maxWidth: "80vw",
     height: "100vh"
@@ -131,148 +122,17 @@ const styles = theme => ({
     textAlign: "right",
     marginTop: 30,
     width: "94%",
-    marginBottom: 20,
+    marginBottom: 0,
     marginLeft: "auto",
     marginRight: "auto"
+  },
+  back: {
+    textDecoration: "none",
+    fontSize: "1.2em"
   },
   submit: {
     fontSize: "2em"
   }
-=======
-const styles = (theme) => ({
-   registerContainer: {
-      maxWidth: "80vw",
-      height: "100vh"
-   },
-   registerFormContainer: {
-      width: "60vw",
-      margin: 0,
-      marginLeft: "auto",
-      marginRight: "auto",
-      border: "1px solid black",
-      padding: 10,
-      marginBottom: 30
-   },
-   logo: {
-      width: "8vw"
-   },
-   flexContainer: {
-      display: "flex",
-      fontSize: "3em",
-      justifyContent: "space-around"
-   },
-   postalFlex: {
-      display: "flex",
-      width: "100%"
-   },
-   needFlex: {
-      display: "flex",
-      flexFlow: "column",
-      width: "45%",
-      justifyContent: "space-between"
-   },
-   postalCode: {
-      width: "50%",
-      fontSize: "0.4em",
-      height: "3em",
-      paddingLeft: 10,
-      marginBottom: 10,
-      marginRight: 10
-   },
-   postalProvince: {
-      width: "50%",
-      fontSize: "0.4em",
-      height: "3em",
-      paddingLeft: 10,
-      marginBottom: 10
-   },
-   field: {
-      fontSize: "0.4em",
-      height: "3em",
-      paddingLeft: 10,
-      marginBottom: 10
-   },
-   buttonsYesNo: {
-      display: "flex",
-      width: "45%",
-      justifyContent: "space-between",
-      margin: 0,
-      marginLeft: "auto",
-      marginRight: "auto"
-   },
-   receiver: {
-      width: "100%",
-      textAlign: "center",
-      marginTop: "2em"
-   },
-   buttonsYes: {
-      width: "8em",
-      height: "3em",
-      borderRadius: "7em",
-      background: "#3051A1",
-      color: "white",
-      display: "flex",
-      alignItems: "center"
-   },
-   yes: {
-      fontSize: "2em"
-   },
-   buttonsNo: {
-      width: "8em",
-      height: "3em",
-      borderRadius: "7em",
-      background: "#2F9F80",
-      color: "white",
-      display: "flex",
-      alignItems: "center"
-   },
-   inputNo: {
-      width: "3em",
-      height: "4em",
-      marginLeft: 10
-   },
-   agreementConteiner: {
-      marginTop: 30,
-      marginBottom: 30,
-      border: "1px solid black",
-      padding: 10,
-      width: "93%",
-      margin: 0,
-      marginLeft: "auto",
-      marginRight: "auto"
-   },
-   checkbox: {
-      width: "100%",
-      textAlign: "right",
-      width: "94%",
-      margin: 0,
-      marginLeft: "auto",
-      marginRight: "auto"
-   },
-   buttonSubmit: {
-      width: "13em",
-      height: "5em",
-      borderRadius: "5em",
-      background: "#3051A1",
-      color: "white"
-   },
-   submitButton: {
-      width: "100%",
-      textAlign: "right",
-      marginTop: 30,
-      width: "94%",
-      marginBottom: 0,
-      marginLeft: "auto",
-      marginRight: "auto"
-   },
-   back: {
-      textDecoration: "none",
-      fontSize: "1.2em"
-   },
-   submit: {
-      fontSize: "2em"
-   }
->>>>>>> 02e6538d364b00c6d65d77f81b0d88fdce9797fb
 });
 
 export default styles;
