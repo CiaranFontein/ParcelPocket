ca6c294f981453ba8120c437c96dfed0c4f3af91
Ciaran Fontein <ciaranfontein@gmail.com>
2019-11-06 16:15:30 -0800

    📍: Map has markers

--- imports/ui/components/RecipientMap/RecipientMap.js
+++ imports/ui/components/RecipientMap/RecipientMap.js
@@ -4,6 +4,8 @@ import React, { Component, Fragment } from "react";
 const API_KEY = Meteor.settings.public.REACT_APP_GOOGLE_API_KEY;
 
 const RecipientMap = ({ user, google, recipients }) => {
+  console.log(user);
+  console.log(recipients);
   //const directionsService = new google.maps.DirectionsService();
   return (
     <Fragment>
