cf7f27ff8816b7c6fa1029466f25242c142ea9b3
Ciaran Fontein <ciaranfontein@gmail.com>
2019-11-07 14:42:50 -0800

    resolved conflict

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,28 +1,28 @@
 if (Meteor.isServer) {
-   Meteor.publish("users", function usersPublication() {
-      return Meteor.users.find({});
-   });
+  Meteor.publish("users", function usersPublication() {
+    return Meteor.users.find({});
+  });
 }
 Meteor.methods({
-   "users.addRating"(id) {
-      let partialScore = Meteor.users
-         .find(id, { profile: { score: 1 } })
-         .fetch()[0];
-      let newScore = partialScore.profile.score + 1;
-      console.log(partialScore);
-      console.log(newScore);
+  "users.addRating"(id) {
+    let partialScore = Meteor.users
+      .find(id, { profile: { score: 1 } })
+      .fetch()[0];
+    let newScore = partialScore.profile.score + 1;
+    console.log(partialScore);
+    console.log(newScore);
 
-      Meteor.users.update(id, {
-         $set: { "profile.score": newScore }
-      });
-   },
-   "users.subtractRating"(id) {
-      let partialScore = Meteor.users
-         .find(id, { profile: { score: 1 } })
-         .fetch()[0];
-      let newScore = partialScore.profile.score - 1;
-      Meteor.users.update(id, {
-         $set: { "profile.score": newScore }
-      });
-   }
+    Meteor.users.update(id, {
+      $set: { "profile.score": newScore }
+    });
+  },
+  "users.subtractRating"(id) {
+    let partialScore = Meteor.users
+      .find(id, { profile: { score: 1 } })
+      .fetch()[0];
+    let newScore = partialScore.profile.score - 1;
+    Meteor.users.update(id, {
+      $set: { "profile.score": newScore }
+    });
+  }
 });
