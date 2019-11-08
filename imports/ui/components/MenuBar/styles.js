a80c0824d65b746a39d433a6932f1f4ccc618ff5
Alex Faissal <alefaissal@gmail.com>
2019-11-08 13:50:38 -0800

    Add status to menubar when on that bage

--- imports/ui/components/MenuBar/styles.js
+++ imports/ui/components/MenuBar/styles.js
@@ -1,73 +1,76 @@
-const styles = theme => ({
-  flexToolbar: {
-    display: "flex",
-    justifyContent: "space-between",
-    width: "90%",
-    marginLeft: 400,
-    marginRight: 400
-  },
-  appBar: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: theme.spacing(2),
-    boxShadow: "none",
-    marginTop: "none",
-    paddingTop: "none"
-  },
-  gravatarimage: {
-    borderRadius: "50%",
-    padding: 20
-  },
-  profiledetails: {
-    display: "flex",
-    border: "0px solid grey",
-    borderRadius: "15px"
-  },
-  profilenamerating: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignContent: "center"
-  },
-  navlinks: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-evenly",
-    paddingLeft: "10px",
-    paddingRight: "10px"
-  },
-  gravatarcontainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  profilename: {
-    margin: "none"
-  },
-  profilerating: {
-    margin: "none"
-  },
-  navlinksbutton: {
-    width: "130px",
-    boxShadow: "none",
-    "&:hover": {
+const styles = (theme) => ({
+   flexToolbar: {
+      display: "flex",
+      justifyContent: "space-between",
+      width: "90%",
+      marginLeft: 400,
+      marginRight: 400
+   },
+   appBar: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginBottom: theme.spacing(2),
       boxShadow: "none",
-      backgroundColor: "grey"
-    }
-  },
-  iconButton: {
-    fontSize: "20px",
-    textTransform: "capitalize"
-  },
-  logoimg: {
-    padding: "20px"
-  },
-  mainlinks: {
-    display: "flex",
-    justifyContent: "space-evenly",
-    width: "25%"
-  }
+      marginTop: "none",
+      paddingTop: "none"
+   },
+   gravatarimage: {
+      borderRadius: "50%",
+      padding: 20
+   },
+   profiledetails: {
+      display: "flex",
+      border: "0px solid grey",
+      borderRadius: "15px"
+   },
+   profilenamerating: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignContent: "center"
+   },
+   navlinks: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-evenly",
+      paddingLeft: "10px",
+      paddingRight: "10px"
+   },
+   gravatarcontainer: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center"
+   },
+   profilename: {
+      margin: "none"
+   },
+   profilerating: {
+      margin: "none"
+   },
+   navlinksbutton: {
+      width: "130px",
+      boxShadow: "none",
+      "&:hover": {
+         boxShadow: "none",
+         backgroundColor: "grey"
+      }
+   },
+   iconButton: {
+      fontSize: "20px",
+      textTransform: "capitalize"
+   },
+   logoimg: {
+      padding: "20px"
+   },
+   mainlinks: {
+      display: "flex",
+      justifyContent: "space-evenly",
+      width: "25%"
+   },
+   active: {
+      borderBottom: "solid 4px #2E9F80"
+   }
 });
 
 export default styles;
