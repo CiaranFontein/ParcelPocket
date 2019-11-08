ad98a0740a7cf0690a2847ab6f655cf4e42ca22d
Alex Faissal <alefaissal@gmail.com>
2019-11-07 16:36:27 -0800

    Fix issue to see the order created

--- imports/ui/components/Recipient/Recipient.js
+++ imports/ui/components/Recipient/Recipient.js
@@ -5,27 +5,28 @@ import Gravatar from "react-gravatar";
 import AddOrder from "../../components/AddOrder";
 
 const Recipient = ({ classes, recipient, currentUserId, transitValue }) => {
-  return recipient ? (
-    <div className={classes.recipientContainer}>
-      <div className={classes.recipientAvatar}>
-        <Gravatar email={recipient.email} />
+   console.log(currentUserId);
+   return recipient ? (
+      <div className={classes.recipientContainer}>
+         <div className={classes.recipientAvatar}>
+            <Gravatar email={recipient.email} />
+         </div>
+         <div className={classes.recipientName}>
+            {recipient.profile.firstName} {recipient.profile.lastName}
+         </div>
+         <div className={classes.recipientAddress}>
+            {recipient.profile.address}
+         </div>
+         <div className={classes.recipientScore}>{recipient.profile.score}</div>
+         <AddOrder
+            currentUserId={currentUserId}
+            recipient={recipient}
+            transitValue={transitValue}
+         />
       </div>
-      <div className={classes.recipientName}>
-        {recipient.profile.firstName} {recipient.profile.lastName}
-      </div>
-      <div className={classes.recipientAddress}>
-        {recipient.profile.address}
-      </div>
-      <div className={classes.recipientScore}>{recipient.profile.score}</div>
-      <AddOrder
-        currentUserId={currentUserId}
-        recipient={recipient}
-        transitValue={transitValue}
-      />
-    </div>
-  ) : (
-    ""
-  );
+   ) : (
+      ""
+   );
 };
 
 export default withStyles(styles)(Recipient);
--- imports/ui/pages/Recipients/Recipients.js
+++ imports/ui/pages/Recipients/Recipients.js
@@ -7,81 +7,86 @@ import RecipientMap from "../../components/RecipientMap";
 import Recipient from "../../components/Recipient";
 import { withTracker } from "meteor/react-meteor-data";
 
-const Recipients = ({ classes, recipients, currentUser }) => {
-  const [transitValue, setTransitValue] = React.useState(10);
-  const [distanceValue, setDistanceValue] = React.useState(10);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  return (
-    <div className={classes.recipientsPage}>
-      <div className={classes.recipientsPageContainer}>
-        Recipients
-        <div className={classes.optionsPanel}>
-          <div className={classes.deliveryWindow}>
-            <Typography id="max-transit-time-slider" gutterBottom>
-              Max Transit Time (days)
-            </Typography>
-            <Slider
-              value={transitValue}
-              defaultValue={14}
-              aria-labelledby="Max Transit Time (days)"
-              valueLabelDisplay="auto"
-              step={1}
-              min={0}
-              max={100}
-              onChange={(event, value) => setTransitValue(value)}
-            />
-          </div>
-          <div className={classes.maxRange}>
-            <Typography id="max-range-slider" gutterBottom>
-              Max Range (km)
-            </Typography>
-            <Slider
-              value={distanceValue}
-              defaultValue={10}
-              aria-labelledby="Max Range (km)"
-              valueLabelDisplay="auto"
-              step={1}
-              min={0}
-              max={50}
-              onChange={(event, value) => setDistanceValue(value)}
-            />
-          </div>
-        </div>
-        <div className={classes.recipientMapContainer}>
-          {recipients.length > 0 && (
-            <RecipientMap
-              user={currentUser}
-              recipients={recipients}
-              transitValue={transitValue}
-            />
-          )}
-        </div>
-        <div className={classes.recipientListContainer}>
-          {recipients.length > 0 &&
-            recipients.map((user, index) => (
-              <Recipient
-                key={index}
-                recipient={user}
-                transitValue={transitValue}
-              />
-            ))}
-        </div>
+const Recipients = ({ classes, recipients, currentUser, currentUserId }) => {
+   console.log(currentUser);
+   const [transitValue, setTransitValue] = React.useState(10);
+   const [distanceValue, setDistanceValue] = React.useState(10);
+   const handleChange = (event, newValue) => {
+      setValue(newValue);
+   };
+   return (
+      <div className={classes.recipientsPage}>
+         <div className={classes.recipientsPageContainer}>
+            Recipients
+            <div className={classes.optionsPanel}>
+               <div className={classes.deliveryWindow}>
+                  <Typography id="max-transit-time-slider" gutterBottom>
+                     Max Transit Time (days)
+                  </Typography>
+                  <Slider
+                     value={transitValue}
+                     defaultValue={14}
+                     aria-labelledby="Max Transit Time (days)"
+                     valueLabelDisplay="auto"
+                     step={1}
+                     min={0}
+                     max={100}
+                     onChange={(event, value) => setTransitValue(value)}
+                  />
+               </div>
+               <div className={classes.maxRange}>
+                  <Typography id="max-range-slider" gutterBottom>
+                     Max Range (km)
+                  </Typography>
+                  <Slider
+                     value={distanceValue}
+                     defaultValue={10}
+                     aria-labelledby="Max Range (km)"
+                     valueLabelDisplay="auto"
+                     step={1}
+                     min={0}
+                     max={50}
+                     onChange={(event, value) => setDistanceValue(value)}
+                  />
+               </div>
+            </div>
+            <div className={classes.recipientMapContainer}>
+               {recipients.length > 0 && (
+                  <RecipientMap
+                     user={currentUser}
+                     recipients={recipients}
+                     transitValue={transitValue}
+                  />
+               )}
+            </div>
+            <div className={classes.recipientListContainer}>
+               {recipients.length > 0 &&
+                  recipients.map((user, index) => (
+                     <Recipient
+                        key={index}
+                        recipient={user}
+                        currentUserId={currentUserId}
+                        transitValue={transitValue}
+                     />
+                  ))}
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default withTracker(() => {
-  Meteor.subscribe("users");
-  return {
-    recipients: Meteor.users
-      .find({
-        $and: [{ _id: { $ne: Meteor.userId() } }, { "profile.reciever": true }]
-      })
-      .fetch(),
-    currentUser: Meteor.user(),
-    currentUserId: Meteor.userId()
-  };
+   Meteor.subscribe("users");
+   return {
+      recipients: Meteor.users
+         .find({
+            $and: [
+               { _id: { $ne: Meteor.userId() } },
+               { "profile.reciever": true }
+            ]
+         })
+         .fetch(),
+      currentUser: Meteor.user(),
+      currentUserId: Meteor.userId()
+   };
 })(withStyles(styles)(Recipients));
